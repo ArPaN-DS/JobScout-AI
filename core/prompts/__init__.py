d1d9435435567eaa3756b09a48064e83d3255873
ArPaN-DS <arpanmajumdar952@gmail.com>
2026-05-31 20:28:06 +0530

    chore: update prompts package init

---
+++ core/prompts/__init__.py
@@ -0,0 +1,9 @@
+from .registry import PROMPT_VERSION, build_application_kit_prompt, build_critic_prompt, build_job_match_prompt, build_profile_extract_prompt
+
+__all__ = [
+    "PROMPT_VERSION",
+    "build_profile_extract_prompt",
+    "build_job_match_prompt",
+    "build_application_kit_prompt",
+    "build_critic_prompt",
+]
